c3636da52f53010ed9cdec89c14cef861b86526c
nagamoto <koutaro.n.1056@gmail.com>
2023-04-14 00:27:22 +0900

    ResourceNotFoundExceptionの修正

--- src/test/java/com/raisetech/crudTask/junit/service/CoServiceImplTest.java
+++ src/test/java/com/raisetech/crudTask/junit/service/CoServiceImplTest.java
@@ -55,15 +55,6 @@ public class CoServiceImplTest {
     @Test
     @DisplayName("存在しないIDを指定したときにResourceNotFoundExceptionがスローされること")
     public void ThrowResourceNotFoundException() {
-        when(coMapper.findById(100)).thenReturn(Optional.empty());
-
-        assertThrows(ResourceNotFoundException.class, () -> coMapper.findById(100));
-        verify(coMapper, times(1)).findById(100);
-    }
-
-    @Test
-    @DisplayName("存在しないIDを指定したときにResourceNotFoundExceptionがスローされること（仮）")
-    public void ThrowResourceNotFoundException2() {
         doReturn(Optional.empty()).when(coMapper).findById(100);
         assertThrows(ResourceNotFoundException.class, () -> coServiceImpl.findById(100));
     }
